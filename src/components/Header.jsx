c12b70be518dfcfbcfb0a475b346292dfd51765d
AdalbertoAo <4dalbertosil@gmail.com>
2025-08-07 17:27:51 +0100

    corriguindo caminho da logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="flex flex-col items-center text-center w-full">
       <div className="flex items-center justify-center gap-x-3 mb-2">
-        <img src="public\logo_dark-removebg.png" alt="" className="w-2xl h-2xl"/>
+        <img src="/logo_dark-removebg.png" alt="" className="w-2xl h-2xl"/>
       </div>
       <p className="text-gray-200 mt-8 text-base text-center max-w-xs leading-relaxed">
        Abrimos portas, Transformamos vidas.
